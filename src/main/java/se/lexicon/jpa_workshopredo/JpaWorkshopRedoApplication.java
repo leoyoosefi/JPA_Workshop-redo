2747012c338d1490a7c597fd02fa90c473fb4d81
Leo Yoosefi <leoyoosefi@gmail.com>
2022-07-27 19:27:18 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/jpa_workshopredo/JpaWorkshopRedoApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.jpa_workshopredo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWorkshopRedoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaWorkshopRedoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/jpa_workshopredo/JpaWorkshopRedoApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.jpa_workshopredo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWorkshopRedoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
